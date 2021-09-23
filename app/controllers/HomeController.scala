2adfd9764b1dd4a0f36c8a3d818360d5033869a3
Pedro Flores <pdavidflores@gmail.com>
2021-09-23 13:33:19 -0400

    example code for stack-builders

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,46 @@
+package controllers
+
+import scala.language.postfixOps
+import concurrent.Await
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import javax.inject.Inject
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import play.api.mvc._
+import play.api.libs.ws._
+import play.api.http.HttpEntity
+import akka.actor.ActorSystem
+import akka.stream.scaladsl._
+import akka.util.ByteString
+
+import scala.concurrent.ExecutionContext
+
+import play.api.libs.concurrent.CustomExecutionContext
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(implicit ec: ExecutionContext, ws: WSClient, val controllerComponents: ControllerComponents) extends BaseController {
+
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index() =  Action.async { 
+    val resp = ws.url("https://api.github.com/users/neowinx/repos").get()
+    Future {
+       val popo = Await.result(resp, 15 seconds)
+       Ok(popo.json)
+    }
+  }
+}
